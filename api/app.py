bf24d8fdaa1568239c78ca1e523c2da8a87b3588
Alexandria Redmon <alexandriadredmon@gmail.com>
2022-01-23 16:25:48 -0500

    Transparent overlays

--- api/app.py
+++ api/app.py
@@ -15,10 +15,10 @@ def root():
         "status": "ok",
     })
 
-@app.route('/upload/', methods=["POST"])
+@app.route('/upload', methods=["POST"])
 def upload():
     (response, response_code, headers) = action(request)
     return jsonify(response), response_code
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True, use_reloader=True)
--- api/main.py
+++ api/main.py
@@ -5,6 +5,8 @@ from PIL import Image, ImageDraw
 NOTCH_HEIGHT = 65
 TARGET_DEVICE_WIDTH = 3024
 TARGET_DEVICE_HEIGHT = 1964
+TINT_COLOR = (0, 0, 0)
+OPACITY = int(255 * .75)
 
 def action(request):
     if request.method != "POST":
@@ -17,6 +19,7 @@ def action(request):
         return ('', 204, headers)
     file = request.files["file"]
     image = Image.open(io.BytesIO(file.read()))
+    image = image.convert("RGBA")
     size = image.size
     width = size[0]
     height = size[1]
@@ -26,19 +29,28 @@ def action(request):
         new_height = (width * TARGET_DEVICE_HEIGHT) / TARGET_DEVICE_WIDTH
         new_height_pos = (height - new_height) / 2
         image = image.crop((0, new_height_pos, width, new_height_pos + new_height))
-        height = new_height
+        height = int(new_height)
     else:
         # Cropping horizontally
         new_width = (height * TARGET_DEVICE_WIDTH) / TARGET_DEVICE_HEIGHT
         new_width_pos = (width - new_width) / 2
         image = image.crop((new_width_pos, 0, new_width_pos + new_width, height))
-        width = new_width
-        pass
+        width = int(new_width)
 
     notch_height = (NOTCH_HEIGHT * height) / TARGET_DEVICE_HEIGHT
-    rectangle = [(0, 0), width, notch_height]
-    masked = ImageDraw.Draw(image)
-    masked.rectangle(rectangle, fill="#00000088")
+
+    overlay = Image.new('RGBA', image.size, TINT_COLOR+(0,))
+    draw = ImageDraw.Draw(overlay)
+    draw.rectangle(
+        (
+            (0, 0),
+            (width, notch_height)
+        ),
+        fill=TINT_COLOR+(OPACITY,)
+    )
+
+    image = Image.alpha_composite(image, overlay)
+    image = image.convert("RGB")
 
     modified = io.BytesIO()
     image.save(modified, format='JPEG')
