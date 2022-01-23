48bcbed9ad8d1c2be437e3863fdef8f71ab77dae
Alexandria Redmon <alexandriadredmon@gmail.com>
2022-01-23 00:28:54 -0500

    Functional POC

--- api/main.py
+++ api/main.py
@@ -1,8 +1,61 @@
-from fastapi import FastAPI
+import base64
+from cmath import rect
+from fastapi import FastAPI, File
+from fastapi.middleware.cors import CORSMiddleware
+
+from PIL import Image, ImageDraw
+import io
 
 app = FastAPI()
-
+origins = [
+    "http://localhost:3000"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"status": "OK"}
+
+NOTCH_HEIGHT = 65
+TARGET_DEVICE_WIDTH = 3024
+TARGET_DEVICE_HEIGHT = 1964
+
+@app.post("/upload/")
+async def upload(file: bytes = File(...)):
+    image = Image.open(io.BytesIO(file))
+    print(image.size)
+    size = image.size
+    width = size[0]
+    height = size[1]
+    ratio = width / height
+    if ratio < 1.54:
+        # Cropping vertically
+        new_height = (width * TARGET_DEVICE_HEIGHT) / TARGET_DEVICE_WIDTH
+        new_height_pos = (height - new_height) / 2
+        image = image.crop((0, new_height_pos, width, new_height_pos + new_height))
+        height = new_height
+    else:
+        # Cropping horizontally
+        new_width = (height * TARGET_DEVICE_WIDTH) / TARGET_DEVICE_HEIGHT
+        new_width_pos = (width - new_width) / 2
+        image = image.crop((new_width_pos, 0, new_width_pos + new_width, height))
+        width = new_width
+        pass
+
+    notch_height = (NOTCH_HEIGHT * height) / TARGET_DEVICE_HEIGHT
+    rectangle = [(0, 0), width, notch_height]
+    masked = ImageDraw.Draw(image)
+    masked.rectangle(rectangle, fill="#00000088")
+
+    modified = io.BytesIO()
+    image.save(modified, format='JPEG')
+
+    return {
+        "image": base64.b64encode(modified.getvalue()),
+    }
--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,41 @@
-import './App.css';
+import { useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+import If from './util'
 
 function App() {
+  const [image, setImage] = useState()
+  const handleSelectFile = e => {
+    const file = e.target.files[0]
+    if (! file) {
+      return false
+    }
+    let formData = new FormData()
+    formData.append(
+      'file',
+      file,
+      file.name,
+    )
+    const headers = {
+      "Content-Type": file.type,
+    }
+    axios.post(
+      'http://localhost:7001/upload/',
+      formData,
+      headers,
+    ).then(response => {
+      const image = response.data.image
+      setImage(`data:image/jpeg;base64,${image}`)
+      setTimeout(() => {
+        document.getElementById('download-link').click()
+      }, 200)
+    })
+    const uploadInput = document.getElementById('file-upload')
+    if (uploadInput) {
+      uploadInput.value = null
+    }
+  }
   return (
     <div className="App">
       <header>
@@ -15,15 +50,37 @@ function App() {
           alt="Free notchless background generator"
         />
       </header>
-      <img
-        className="AppDemo"
-        src="/img/demo.png"
-        alt=""
-      />
-      <button
-        className="UploadButton"
-        onClick={() => alert('Coming soon!')}
-      ></button>
+      <If condition={true || ! image}>
+        <img
+          className="AppDemo"
+          src="/img/demo.png"
+          alt=""
+        />
+        <input
+          accept="*.csv"
+          style={{ display: 'none' }}
+          id="file-upload"
+          multiple
+          type="file"
+          onChange={e => handleSelectFile(e)}
+        />
+        <label htmlFor="file-upload">
+          <button
+            className="UploadButton"
+            onClick={() => document.getElementById('file-upload').click()}
+          ></button>
+        </label>
+      </If>
+        <a
+          href={image}
+          id="download-link"
+          download
+          style={{
+            display: 'none',
+          }}
+        >
+          Download file
+        </a>
     </div>
   );
 }
